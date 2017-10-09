da9cd8f264fad2b78556a5e39851fdb804d0b609
alperkaracelik <alper.karacelik@gmail.com>
2017-10-09 17:49:37 +0300

    Embedded Tomcat is being tested.

--- src/main/java/com/uber/coding_challange/food_trucks/Main.java
+++ src/main/java/com/uber/coding_challange/food_trucks/Main.java
@@ -1,48 +1,81 @@
 package com.uber.coding_challange.food_trucks;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-/**
- * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
- * command that is used for launching should fire this main method.
- */
-public class Main 
-{
-	public static void main(String[] args) throws Exception
-	{
-		try 
-		{
-			// The port that we should run on can be set into an environment variable
-	        // Look for that variable and default to 8080 if it isn't there.
-	        String webPort = System.getenv("PORT");
-	        if (webPort == null || webPort.isEmpty()) {
-	            webPort = "8080";
-	        }
-	
-	        final Server server = new Server(Integer.valueOf(webPort));
-	        final WebAppContext root = new WebAppContext();
-	
-	        root.setContextPath("/");
-	        // Parent loader priority is a class loader setting that Jetty accepts.
-	        // By default Jetty will behave like most web containers in that it will
-	        // allow your application to replace non-server libraries that are part of the
-	        // container. Setting parent loader priority to true changes this behavior.
-	        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
-	        root.setParentLoaderPriority(true);
-	
-	        final String webappDirLocation = "src/main/webapp/";
-	        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-	        root.setResourceBase(webappDirLocation);
-	
-	        server.setHandler(root);
-	
-	        server.start();
-	        server.join();
-		} 
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-	}
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.WebResourceSet;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.EmptyResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.apache.tomcat.util.scan.Constants;
+import org.apache.tomcat.util.scan.StandardJarScanFilter;
+
+public class Main {
+
+    private static File getRootFolder() {
+        try {
+            File root;
+            String runningJarPath = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().replaceAll("\\\\", "/");
+            int lastIndexOf = runningJarPath.lastIndexOf("/target/");
+            if (lastIndexOf < 0) {
+                root = new File("");
+            } else {
+                root = new File(runningJarPath.substring(0, lastIndexOf));
+            }
+            System.out.println("application resolved root folder: " + root.getAbsolutePath());
+            return root;
+        } catch (URISyntaxException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        File root = getRootFolder();
+        System.setProperty("org.apache.catalina.startup.EXIT_ON_INIT_FAILURE", "true");
+        Tomcat tomcat = new Tomcat();
+        Path tempPath = Files.createTempDirectory("tomcat-base-dir");
+        tomcat.setBaseDir(tempPath.toString());
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+        File webContentFolder = new File(root.getAbsolutePath(), "src/main/webapp/");
+        if (!webContentFolder.exists()) {
+            webContentFolder = Files.createTempDirectory("default-doc-base").toFile();
+        }
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("", webContentFolder.getAbsolutePath());
+        //Set execution independent of current thread context classloader (compatibility with exec:java mojo)
+        ctx.setParentClassLoader(Main.class.getClassLoader());
+
+        System.out.println("configuring app with basedir: " + webContentFolder.getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClassesFolder = new File(root.getAbsolutePath(), "target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+
+        WebResourceSet resourceSet;
+        if (additionWebInfClassesFolder.exists()) {
+            resourceSet = new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClassesFolder.getAbsolutePath(), "/");
+            System.out.println("loading WEB-INF resources from as '" + additionWebInfClassesFolder.getAbsolutePath() + "'");
+        } else {
+            resourceSet = new EmptyResourceSet(resources);
+        }
+        resources.addPreResources(resourceSet);
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 }
